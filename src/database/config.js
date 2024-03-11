50021d39c721448f4a2ffe4bebc760cb0cc667ca
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-03-10 23:32:48 -0300

    finalizado

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("cadastro", "root", "aUTbpSPkeE3izNG33hjrm8E8mw4aaiyh", {
+const sequelize = new Sequelize("cadastro", "ncxxvppq", "aUTbpSPkeE3izNG33hjrm8E8mw4aaiyh", {
     dialect: "mysql",
     host: "postgres://ncxxvppq:aUTbpSPkeE3izNG33hjrm8E8mw4aaiyh@kesavan.db.elephantsql.com/ncxxvppq",
 })
