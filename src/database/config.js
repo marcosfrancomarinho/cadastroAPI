c9c8c315cdde59eb8a4e3edd70c31ae601328532
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-03-10 22:49:17 -0300

    finalizado

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("cadastro", "root", "151822", {
+const sequelize = new Sequelize("cadastro", "root", "*****", {
     dialect: "mysql",
     host: "localhost"
 })
