5db1b3da0871133e76fd7365178773f5aa20b0e6
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-03-11 22:22:29 -0300

    11/03/2024

--- src/database/config.js
+++ src/database/config.js
@@ -1,6 +1,11 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("cadastro", "ncxxvppq", "aUTbpSPkeE3izNG33hjrm8E8mw4aaiyh", {
-    dialect: "postgres",
-    host: "postgres://ncxxvppq:aUTbpSPkeE3izNG33hjrm8E8mw4aaiyh@kesavan.db.elephantsql.com/ncxxvppq",
+require("dotenv").config({ path: __dirname + "/.env" })
+const { Sequelize } = require("sequelize")
+const sequelize = new Sequelize({
+    database: process.env.DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect:"postgres"
 })
+
 module.exports = sequelize
